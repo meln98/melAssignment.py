fa70733418117498145b0fa43a66be5eaf051f19
meln98 <87532131+meln98@users.noreply.github.com>
2021-07-29 16:21:23 +0800

    Update melAssignment.py

--- melAssignment.py
+++ melAssignment.py
@@ -103,25 +103,25 @@ else:
 # analyze = sv.analyze(data)
 # st.write(analyze)
 
-# def plotCorrelationMatrix(data, graphWidth):
-#     filename = data
-#     # filename = data.DataFrame
-#     df = data.dropna() # drop columns & rows with NaN
-#     df = data[[col for col in data if data[col].nunique() > 1]] # keep columns where there are more than 1 unique values
-#     if data.shape[1] < 2:
-#         print(f'No correlation plots shown: The number of non-NaN or constant columns ({data.shape[1]}) is less than 2')
-#         return
-#     corr = df.corr()
-#     plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
-#     corrMat = plt.matshow(corr, fignum = 1)
-#     plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
-#     plt.yticks(range(len(corr.columns)), corr.columns)
-#     plt.gca().xaxis.tick_bottom()
-#     plt.colorbar(corrMat)
-#     plt.title(f'Correlation Matrix for {filename}', fontsize=15)
-#     plt.show()
+def plotCorrelationMatrix(data, graphWidth):
+    filename = data
+    # filename = data.DataFrame
+    df = data.dropna() # drop columns & rows with NaN
+    df = data[[col for col in data if data[col].nunique() > 1]] # keep columns where there are more than 1 unique values
+    if data.shape[1] < 2:
+        print(f'No correlation plots shown: The number of non-NaN or constant columns ({data.shape[1]}) is less than 2')
+        return
+    corr = df.corr()
+    plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
+    corrMat = plt.matshow(corr, fignum = 1)
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    plt.gca().xaxis.tick_bottom()
+    plt.colorbar(corrMat)
+    plt.title(f'Correlation Matrix for {filename}', fontsize=15)
+    plt.show()
 
-# st.write.plotCorrelationMatrix(data, 14)
+st.write.plotCorrelationMatrix(data, 14)
 
 # y = data['Category']
 # y = LabelEncoder().fit_transform(y)
